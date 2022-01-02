8e1d24c420c1c00d89c4569eea9ca2ceb67e3288
ahmed samir sakr <89707420+ahmedsamir010@users.noreply.github.com>
2022-01-03 01:06:47 +0200

    Update Single Linked List.cpp

--- Single Linked List.cpp
+++ Single Linked List.cpp
@@ -1,3 +1,4 @@
+//اذا مررت من هنا فادعوا الله ان ييسر لي امري ويصلح لي شاني
 #include<iostream>
 using namespace std;
 struct node {
@@ -21,23 +22,10 @@ int main()
     linked s;
     s.insertfirst(10);
     s.insertfirst(20);
-   /* s.insertfirst(30);
-    s.insertfirst(40);*/
-    // s.insertfirst(50);
-   //  s.display();// 50  40  30  20  10
-    s.insertpos(90, 20);
-    //  s.insertlast(70);
-    //  s.insertfirst(50);
-     // s.display();// 50 50 40 90 30 20 10 70
-     // s.deltfirst();
-     // s.deltpos(3);
-   // s.deltlast();
-    s.display();//50  40 30  20  10  
-   // s.insertpos(90, 10);
-   // s.display();
+    s.deltpos(1);
+    s.display();
     return 0;
 }
-
 void linked::insertfirst(int value)
 {
     node* newNode = new node;
@@ -53,7 +41,6 @@ void linked::insertfirst(int value)
     }
 
 }
-
 void linked::insertlast(int value)
 {
     node* newNode = new node;
@@ -71,7 +58,6 @@ void linked::insertlast(int value)
         temp->next = newNode;
     }
 }
-
 void linked::insertpos(int value, int pos)
 {
     node* newNode = new node;
@@ -81,15 +67,15 @@ void linked::insertpos(int value, int pos)
         newNode->next = head;
         head = head->next;
         head = newNode;
-    return;
+        return;
     }
-    for (int i = 1;i < pos and cur!=NULL;i++) {
+    for (int i = 1;i < pos and cur != NULL;i++) {
         pre = cur;
         cur = cur->next;
     }
-        if (cur == NULL) {
-      //  cout << "Error" << endl;
-       return;
+    if (cur == NULL) {
+        cout << "Error" << endl;
+        return;
     }
     pre->next = newNode;
     newNode->next = cur;
@@ -101,7 +87,6 @@ void linked::deltfirst()
     head = head->next;
     delete temp;
 }
-
 void linked::deltlast()
 {
     node* rev = new node;
@@ -114,29 +99,34 @@ void linked::deltlast()
     }
     prev->next = NULL;
     delete rev;
-
 }
-
 void linked::deltpos(int pos)
 {
     node* cur = new node;
     node* pre = new node;
     cur = head;pre = NULL;
     if (head == NULL) {
-        delete cur;
         return;
     }
+    if (pos == 1) {
+        cur = head;
+        head = head->next;
+        delete cur;
+    }
     else {
-        for (int i = 1;i < pos;i++)
+        for (int i = 1;i < pos and cur!=NULL;i++)
         {
             pre = cur;
             cur = cur->next;
+        }
+        if (cur == NULL) {
+            //cout << "Error";
+            return;
         }
         pre->next = cur->next;
         delete cur;
     }
 }
-
 void linked::display()
 {
     node* cur = new node;
